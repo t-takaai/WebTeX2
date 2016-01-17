aee0d70a4f33f6c32d9b84b31bae2f108df92b59
t-takaai <takoyaj@gmail.com>
2016-01-17 23:41:26 +0900

    rack app style

--- main.rb
+++ main.rb
@@ -1,29 +1,42 @@
 # パッケージの読み込み
 require 'sinatra'
+require 'sinatra/base'
 require 'sinatra/reloader'
 require 'sqlite3'
-require 'securerandom'
+
+class FileUpload < Sinatra::Base
+  configure do
+    enable :static
+    enable :sessions
+  end
 
 # データベースから読み込み
-db = SQLite3::Database.new "db/post.db"
-db.results_as_hash = true # ハッシュとして返し，カラム名でアスセス
+  db = SQLite3::Database.new "db/post.db"
+  db.results_as_hash = true # ハッシュとして返し，カラム名でアスセス
 
-get '/' do
-  posts = db.execute("SELECT file_name FROM posts ORDER BY id DESC")
-  erb :index, { :locals => { :posts => posts } }
-end
+  get '/' do
+    posts = db.execute("SELECT file_name FROM posts ORDER BY id DESC")
+    erb :index, { :locals => { :posts => posts } }
+  end
 
-post '/' do
-  file_name = ""
+  post '/' do
+    file_name = ""
 
-  if params["file"]
-    tempfile = params[:file][:tempfile]
-    filename = params[:file][:filename]
-    target = "./public/uploads/#{filename}"
+    if params["file"]
+      tempfile = params[:file][:tempfile]
+      filename = params[:file][:filename]
+      target = "./public/uploads/#{filename}"
 
-    File.open(target, 'wb') {|f| f.write tempfile.read }
+      File.open(target, 'wb') do |f|
+         f.write tempfile.read
+      end
+
+    else
+      return "ファイルが必須です"
+    end
+
     redirect '/'
-  else
-    return "ファイルが必須です"
   end
 end
+
+FileUpload.run!
